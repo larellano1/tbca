a3d6f1d0a96a3b00c5f498799a776d6486e7c74f
Luis Felipe Vidal Arellano <d805664@SF.PREFEITURA.SP.GOV.BR>
2018-08-20 19:03:54 -0300

    analises

--- tbca.py
+++ tbca.py
@@ -6,28 +6,33 @@ import bs4
 from selenium import webdriver
 import time
 
-url = "http://www.nware.com.br/tbca/tbca/"
 
-browser = webdriver.Chrome("chromedriver.exe") #replace with .Firefox(), or with the browser of your choice
-browser.get(url) #navigate to the page
+def baixar_tabela():
+        
+    url = "http://www.nware.com.br/tbca/tbca/"
 
-produtos = pd.read_csv("lista_alimentos.csv", encoding='latin1', sep=";")
+    browser = webdriver.Chrome("chromedriver.exe") #replace with .Firefox(), or with the browser of your choice
+    browser.get(url) #navigate to the page
 
-tabelas = []
+    produtos = pd.read_csv("lista_alimentos.csv", encoding='latin1', sep=";")
 
-for cod in produtos["Cod"]:
-    produto = produtos[produtos["Cod"] == cod]["Produto"]
-    detalhe = browser.execute_script("getProdMedida('{0}')".format(cod))
-    time.sleep(1)
-    innerHtml = browser.execute_script("return document.body.innerHTML")
-    soup = bs4.BeautifulSoup(innerHtml)
-    table = pd.read_html(str(soup.find(id="itproduto")))[0]
-    table = table.iloc[:,:3]
-    table["Cod"] = cod
-    table["Produto"] = produto
-    tabelas.append(table)
+    tabelas = []
 
-df = pd.concat(tabelas)
-pd.to_pickle(df,"tbca.pickle")
+    for cod in produtos["Cod"]:
+        produto = produtos[produtos["Cod"] == cod]["Produto"]
+        detalhe = browser.execute_script("getProdMedida('{0}')".format(cod))
+        time.sleep(1)
+        innerHtml = browser.execute_script("return document.body.innerHTML")
+        soup = bs4.BeautifulSoup(innerHtml)
+        table = pd.read_html(str(soup.find(id="itproduto")))[0]
+        table = table.iloc[:,:3]
+        table["Cod"] = cod
+        table["Produto"] = produto
+        tabelas.append(table)
 
-print(df)
+    df = pd.concat(tabelas)
+    pd.to_pickle(df,"tbca.pickle")
+
+
+df = pd.read_pickle("tbca.pickle")
+print(df[(df['Componente'] == 'Energia') & (df['Unidades'] == 'kcal')].pivot_table(index=['Cod'], values='Valor por 100 g', aggfunc='sum'))
